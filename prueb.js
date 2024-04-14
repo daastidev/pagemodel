0da9fcb4c51b9a47572d906f4bc34c191eb7f473
daasTI_dev <71340085+daastidev@users.noreply.github.com>
2024-04-13 20:31:56 -0500

    Add files via upload

---
+++ prueb.js
@@ -0,0 +1,41 @@
+var a= 1;
+var b= 2;
+var c= 3;
+var d= 4;
+var e= 5;
+var f= 6;
+var g= 7;
+var h= 8;
+var i= 9;
+var j= 10;
+
+var entradaverify = window.prompt("Enter the Option")
+if (entradaverify === 1 ) {
+  println({
+    "id":0,
+    "Sector_Compania":"Comunicaciones fijas, opticas, móviles e inalámbricas",
+    "Nombre_Compania":"AT& T",        
+    "Cifra_de_Negocio":"$168.9 billion",
+    "Año_Fundación":"1983",
+    "Sede":"Dallas, Texas, USA",
+    "TelecomServices_Paises":" EE.UU. y varios países de América Latina y el Caribe",
+    "Marcas_SubmarcasOwnwer":"AT&T (marca principal), Cricket Wireless",
+    "Posicion_Actual":"1"              
+});
+    
+  } else if (entradaverify === b ) {
+    println({
+        "id":0,
+        "Sector_Compania":"Comunicaciones fijas, opticas, móviles e inalámbricas",
+        "Nombre_Compania":"AT& T",        
+        "Cifra_de_Negocio":"$168.9 billion",
+        "Año_Fundación":"1983",
+        "Sede":"Dallas, Texas, USA",
+        "TelecomServices_Paises":" EE.UU. y varios países de América Latina y el Caribe",
+        "Marcas_SubmarcasOwnwer":"AT&T (marca principal), Cricket Wireless",
+        "Posicion_Actual":"1"              
+    });
+  } else {
+    /* do something */
+
+  }
